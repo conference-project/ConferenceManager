2eb51f9199ea704e255358ea29f7e4311d2f9049
Tomasz Skora <tskora93@gmail.com>
2018-05-04 15:09:29 +0200

    Usunąłem kaskadowość z adnotacji ManyToOne a klasie Article. To ważne, bo ta kaskadowość powodowała, że usunięcie artykułu powodowało usunięcie uczestnika (a to powodowało usunięcie jego innych artykułów).

--- src/main/java/pl/edu/agh/ki/mwo/model/Article.java
+++ src/main/java/pl/edu/agh/ki/mwo/model/Article.java
@@ -21,7 +21,7 @@ public class Article implements java.io.Serializable {
 	@Column
 	private String attachment;
 	
-	@ManyToOne(cascade = {CascadeType.ALL})
+	@ManyToOne
 	@JoinColumn(name="participant_id")
 	private Participant participant;
 	

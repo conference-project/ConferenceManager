b457ef550ecfd965f89458f68e180a0a1e17c38c
Tomasz Skora <tskora93@gmail.com>
2018-05-07 21:58:57 +0200

    Limit rozmiarowy pliku powiększony do 10MB (za pośrednictwem dodatkowego pliku application.properties)

--- src/main/java/pl/edu/agh/ki/mwo/web/app/Application.java
+++ src/main/java/pl/edu/agh/ki/mwo/web/app/Application.java
@@ -19,8 +19,6 @@ import pl.edu.agh.ki.mwo.persistence.DatabaseConnector;
 @ComponentScan(basePackages = "pl.edu.agh.ki.mwo.web.controllers")
 public class Application {
 	
-	//private int maxUploadSizeInMb = 10 * 1024 * 1024; // 10 MB
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -28,23 +26,6 @@ public class Application {
     @PreDestroy
     public void stop() {
        DatabaseConnector.getInstance().teardown();
-    }
-    
-    //Tomcat large file upload connection reset
-    //http://www.mkyong.com/spring/spring-file-upload-and-connection-reset-issue/
-    /*@Bean
-    public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
-
-    	TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
-
-    	tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
-    		if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
-    			//-1 means unlimited
-    			((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
-    		}
-    	});
-        
-    	return tomcat;
-    }*/
+    }    
     
 }
